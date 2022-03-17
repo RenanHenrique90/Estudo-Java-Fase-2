4ee5a07bb33fa84e395f3e72fbc9873c30afeac6
Renan Henrique <renan.henri90@icloud.com>
2022-03-17 16:14:45 -0300

    Usando metodos Getters end Setters

--- EstudoJava2/src/cursojava2/classes/Aluno.java
+++ EstudoJava2/src/cursojava2/classes/Aluno.java
@@ -1,8 +1,9 @@
 package cursojava2.classes;
 
-//Esta é nossa classe/objeto que representa o Aluno
+/*Esta é nossa classe/objeto que representa o Aluno
+ *Ela contem os atribudos dos Objetos instanciados/referenciados */
 public class Aluno {
-	
+
 	String nome;
 	int idade;
 	String dataNascimento;
@@ -13,22 +14,93 @@ public class Aluno {
 	String dataMatricula;
 	String nomeEscola;
 	String serieMatriculado;
-	
-	//objeto que não retorna parametros padrão definido
+
+	// objeto que não retorna parametros padrão definido
 	public Aluno() {
-		
+
 	}
-	
-	//objeto que recebe parametros padrão definido (String)
+
+	// objeto que recebe parametros padrão definido (String)
 	public Aluno(String nomePadrao) {
-		nome =  nomePadrao;
+		nome = nomePadrao;
 	}
-	
-	//objeto que recebe dois parametros padrão (String, int)
+
+	// objeto que recebe dois parametros padrão (String, int)
 	public Aluno(String nomePadrao, int idadePadrao) {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
 }
-
-
--- EstudoJava2/src/cursojava2/executavel/OrientacaoObj.java
+++ EstudoJava2/src/cursojava2/executavel/OrientacaoObj.java
@@ -6,15 +6,76 @@ public class OrientacaoObj {
     
 	public static void main(String[] args) {
 		
-		
+		/* aluno1, aluno2, aluno3 e aluno4, são referencias do Objeto criado e,
+		 * new Aluno(); é a instancia (criação do objeto da referencia) 
+		 */
 		Aluno aluno1 = new Aluno();//Sem parametros definodos
+		aluno1.setNome("Renan Henrique");
+		aluno1.setIdade(31);
+		aluno1.setDataNascimento("03/09/1990");
+		aluno1.setRegistroGeral("4892384928");
+		aluno1.setNumeroCpf("98392849");
+		aluno1.setNomeMae("Guilhermina");
+		aluno1.setNomePai("Jorge");
+		aluno1.setDataMatricula("13/03/2022");
+		aluno1.setNomeEscola("JDEV Treinamento");
+		aluno1.setSerieMatriculado("Quinta Serie");
 		
+		System.out.println("Nome: " + aluno1.getNome());
+		System.out.println("Idade: " + aluno1.getIdade());
+		System.out.println("Data nascimento: " + aluno1.getDataNascimento());
+		
+		//==================================================================================
+		System.out.println("-----------------------------------------------------");
 		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Julia");
+		aluno2.setIdade(22);
+		aluno2.setDataNascimento("27/04/1999");
+		aluno2.setRegistroGeral("4892384928");
+		aluno2.setNumeroCpf("98392849");
+		aluno2.setNomeMae("Joana");
+		aluno2.setNomePai("Jonas");
+		aluno2.setDataMatricula("13/03/2022");
+		aluno2.setNomeEscola("JDEV Treinamento");
+		aluno2.setSerieMatriculado("Quinta Serie");
 		
+		System.out.println("Nome: " + aluno2.getNome());
+		System.out.println("Idade: " + aluno2.getIdade());
+		System.out.println("Data nascimento: " + aluno2.getDataNascimento());
+		
+		//==================================================================================
+		System.out.println("-----------------------------------------------------");
 		Aluno aluno3 = new Aluno("Maria");//Recebendo parametros padrão (String)
+		aluno3.setIdade(30);
+		aluno3.setDataNascimento("03/09/1992");
+		aluno3.setRegistroGeral("4892384928");
+		aluno3.setNumeroCpf("98392849");
+		aluno3.setNomeMae("Guilhermina");
+		aluno3.setNomePai("Jorge");
+		aluno3.setDataMatricula("13/03/2022");
+		aluno3.setNomeEscola("JDEV Treinamento");
+		aluno3.setSerieMatriculado("Quinta Serie");
 		
-		Aluno aluno4 = new Aluno("João", 45);//Recebendo parametros padrão (String, int)
+		System.out.println("Nome: " + aluno3.getNome());
+		System.out.println("Idade: " + aluno3.getIdade());
+		System.out.println("Data nascimento: " + aluno3.getDataNascimento());
 		
+		//==================================================================================
+		System.out.println("-----------------------------------------------------");
+		Aluno aluno4 = new Aluno("João", 28);//Recebendo parametros padrão (String, int)
+		aluno4.setDataNascimento("17/05/1993");
+		aluno4.setRegistroGeral("4892384928");
+		aluno4.setNumeroCpf("98392849");
+		aluno4.setNomeMae("Guilhermina");
+		aluno4.setNomePai("Jorge");
+		aluno4.setDataMatricula("13/03/2022");
+		aluno4.setNomeEscola("JDEV Treinamento");
+		aluno4.setSerieMatriculado("Quinta Serie");
+		
+		System.out.println("Nome: " + aluno4.getNome());
+		System.out.println("Idade: " + aluno4.getIdade());
+		System.out.println("Data nascimento: " + aluno4.getDataNascimento());
+	
 	}
 	
 }
