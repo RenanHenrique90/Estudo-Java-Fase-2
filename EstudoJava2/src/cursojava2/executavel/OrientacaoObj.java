212091a9dabb034042b8fbb3afb5c39ac50c76af
Renan Henrique <renan.henri90@icloud.com>
2022-03-17 12:29:54 -0300

    Aprendendo a criar um objeto sem e com parametros.

--- EstudoJava2/src/cursojava2/executavel/OrientacaoObj.java
+++ EstudoJava2/src/cursojava2/executavel/OrientacaoObj.java
@@ -1,5 +1,20 @@
 package cursojava2.executavel;
 
-public class OrientacaoObj {
+import cursojava2.classes.Aluno;
 
+public class OrientacaoObj {
+    
+	public static void main(String[] args) {
+		
+		
+		Aluno aluno1 = new Aluno();//Sem parametros definodos
+		
+		Aluno aluno2 = new Aluno();
+		
+		Aluno aluno3 = new Aluno("Maria");//Recebendo parametros padrão (String)
+		
+		Aluno aluno4 = new Aluno("João", 45);//Recebendo parametros padrão (String, int)
+		
+	}
+	
 }
